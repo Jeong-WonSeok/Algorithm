2bc4deefb03243d247d7b2c9c60b74e4c8c1334e
Jeong-Wonseok <jws4858@gmail.com>
2023-06-05 02:22:29 +0900

    0605 Algo

--- BAEKJOON/src/GOLD/TWO/새로운게임2_17837/Main.java
+++ BAEKJOON/src/GOLD/TWO/새로운게임2_17837/Main.java
@@ -3,9 +3,7 @@ package GOLD.TWO.새로운게임2_17837;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
@@ -15,9 +13,9 @@ public class Main {
     static int[] dy = {0, 0, 0, -1, 1};
 
     // 흰:0, 빨:1, 파:2
-    static int[][] map;
-    static chessPiece[] index;
-    static Deque<chessPiece>[][] piece;
+    static int[][] map,count;
+    static ArrayList<chessPiece> index;
+    static chessPiece[][] piece;
     static class chessPiece{
         int x, y;
         int dir, idx;
@@ -37,14 +35,13 @@ public class Main {
         K = Integer.parseInt(st.nextToken());
         isEnd = false;
         map = new int[N][N];
-        piece = new Deque[N][N];
-        index = new chessPiece[K];
+        count = new int[N][N];
+        index = new ArrayList<>();
 
-        for(int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < N; j++){
+            for (int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                piece[i][j] = new ArrayDeque<>();
             }
         }
         for (int i = 0; i < K; i++) {
@@ -52,138 +49,116 @@ public class Main {
             int y = Integer.parseInt(st.nextToken()) - 1;
             int x = Integer.parseInt(st.nextToken()) - 1;
             int dir = Integer.parseInt(st.nextToken());
-            piece[y][x].offerFirst(new chessPiece(x,y,dir,i));
-            index[i] = new chessPiece(x,y,dir,i);
+            count[y][x]++;
+            index.add(new chessPiece(x, y, dir, count[y][x]));
         }
-        boolean isEnd = false;
+
         int turn = 0;
         while(true){
             turn++;
             for(int i = 0; i < K; i++){
-                isEnd = move(i);
+                chessPiece cur = index.get(i);
+                move(cur.x, cur.y, cur.dir, cur.idx);
             }
-            for(int i = 0; i < K; i++){
-                System.out.println("idx " + i + " x " + index[i].x + " y " +index[i].y);
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < N; j++){
+                    if(map[i][j] >= 4){
+                        System.out.println(turn);
+                        System.exit(0);
+                    }
+                }
             }
-            System.out.println();
-            if(isEnd) {
-                break;
-            }
-            if(turn >= 1000) {
+            if(turn > 1000) {
                 turn = -1;
                 break;
             }
         }
 
-        System.out.println(turn);
-
     }
-
-    static private boolean move(int idx){
-        chessPiece cur = index[idx];
-        int dir = cur.dir;
-        int ny = cur.y + dy[dir];
-        int nx = cur.x + dx[dir];
-
-        if(nx < 0 || nx >= N || ny < 0 || ny >= N) {
-            goBlue(cur, nx, ny, idx);
-            return false;
-        }
-
-        if(map[ny][nx] == 0){
-            return goWhite(cur, nx, ny, idx);
+    
+    
+    // x, y 현재 위치
+    // dir 방향
+    // idx 이동하는 첫번째
+    private static void move(int x, int y, int dir, int idx) {
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+        if(nx < 0 || nx >= N || ny < 0 || ny >= N || map[ny][nx] == 2){
+            Blue(x, y, dir, idx);
         }else if(map[ny][nx] == 1){
-            return goRed(cur, nx, ny, idx);
-        }else{
-            goBlue(cur, nx, ny, idx);
+            Red(x, y, dir, idx);
+        }else {
+            White(x, y, dir, idx);
         }
-        return false;
     }
 
-    // 이동하는 곳이 빨간색일 경우
-    // 이동한 후 이동한 말과 그 위의 말의 순서를 반대로 바꾼다.
-    static private boolean goRed(chessPiece cur, int nx, int ny, int idx){
-        int cx = cur.x;
-        int cy = cur.y;
-        //현재 인덱스의 차례가 될 때까지 진행X
-        boolean check = false;
-        int size = piece[cy][cx].size();
-        for(int i = 0; i < size; i++) {
-            chessPiece temp = piece[cy][cx].pollLast();
-            if(temp.idx == idx ) check = true;
-            if(!check) {
-                piece[cy][cx].offer(temp);
-                continue;
+    private static void White(int x, int y, int dir, int idx) {
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+        int cnt = 0;
+        for(int i = 0 ; i < K; i++){
+            int curIdx = index.get(i).idx;
+            int cx = index.get(i).x;
+            int cy = index.get(i).y;
+            int cDir = index.get(i).dir;
+            // 만약 검색한 인덱스가 현재 인덱스보다 크거나 같으면 이동할 차례
+            if(x == cx && y == cy && curIdx >= idx){
+                // 이동할 곳에 +되는 인덱스
+                int tempIdx = curIdx - idx + 1;
+                cnt++;
+                index.set(i, new chessPiece(nx, ny, cDir, map[ny][nx] + tempIdx));
+                map[ny][nx]++;
             }
-            piece[ny][nx].offer(new chessPiece(nx, ny, temp.dir, temp.idx));
-            index[temp.idx] = new chessPiece(nx,ny, temp.dir, temp.idx);
         }
-
-        if(piece[ny][nx].size() >= 4) {
-            return true;
-        }
-
-        return false;
+        //현재 위치 개수 초기화
+        map[y][x] -= cnt;
     }
 
-    // 이동하는 곳이 흰색일 경우
-    // 이동한 후 가장 위에 이동한 말을 놓는다.
-    static private boolean goWhite(chessPiece cur, int nx, int ny, int idx){
-        int cx = cur.x;
-        int cy = cur.y;
-        //현재 인덱스의 차례가 될 때까지 진행X
-        boolean check = false;
-        int size = piece[cy][cx].size();
-        for(int i = 0; i < size; i++) {
-            chessPiece temp = piece[cy][cx].poll();
-            if(temp.idx == idx ) check = true;
-            if(!check) {
-                piece[cy][cx].offer(temp);
-                continue;
+    private static void Red(int x, int y, int dir, int idx) {
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+        int cnt = 0;
+        Queue<chessPiece> que = new LinkedList<>();
+        Queue<Integer> number = new LinkedList<>();
+        for(int i = 0; i < K; i++){
+            int curIdx = index.get(i).idx;
+            int cx = index.get(i).x;
+            int cy = index.get(i).y;
+            int cDir = index.get(i).dir;
+
+            if(x == cx && y == cy && curIdx >= idx){
+                que.offer(new chessPiece(cx, cy, cDir, curIdx));
+                number.offer(i);
+                cnt++;
             }
-            piece[ny][nx].offer(new chessPiece(nx, ny, temp.dir, temp.idx));
-            index[temp.idx] = new chessPiece(nx,ny, temp.dir, temp.idx);
         }
-
-        if(piece[ny][nx].size() >= 4) {
-            return true;
+        while(!que.isEmpty()){
+            chessPiece temp = que.poll();
+            int tempNum = number.poll();
+            int tempIdx = cnt - temp.idx;
+            index.set(tempNum, new chessPiece(nx, ny, temp.dir, map[ny][nx] + tempIdx));
         }
+        map[y][x] -= cnt;
+        map[ny][nx] += cnt;
 
-        return false;
     }
 
-    // 이동하는 곳이 파란색, 경계 밖일 경우
-    // 반대방향으로 한칸 이동한다.(만약 바꾸고도 파란색일 경우 이동 하지 않는다.)
-    static private void goBlue(chessPiece cur, int nx, int ny, int idx){
-        int cx = cur.x;
-        int cy = cur.y;
-        boolean check = false;
-        int size = piece[cy][cx].size();
-        for(int i = 0; i < size; i++){
-            chessPiece temp = piece[cy][cx].poll();
-            if(temp.idx == idx ) check = true;
-            if(!check) {
-                piece[cy][cx].offer(temp);
-                continue;
-            }
-            int curDir = 0;
-            if(temp.dir == 1 || temp.dir == 3) curDir = temp.dir + 1;
-            else curDir = temp.dir - 1;
-            nx = temp.x + dx[curDir];
-            ny = temp.y + dy[curDir];
-
-            if( nx < 0 || nx >= N || ny < 0 || ny >= N || map[nx][ny] == 2){
-                piece[cy][cx].offer(new chessPiece(nx, ny, curDir, temp.idx));
-                continue;
-            }
-            if(map[nx][ny] == 1){
-                goRed(new chessPiece(temp.x,temp.y,curDir, temp.idx), nx, ny, temp.idx);
-            }else {
-                goBlue(new chessPiece(temp.x,temp.y,curDir, temp.idx), nx, ny, temp.idx);
-            }
-
+    private static void Blue(int x, int y, int dir, int idx) {
+        if(dir == 1 || dir == 3){
+            dir = dir + 1;
+        }else if(dir == 2 || dir == 4){
+            dir = dir - 1;
         }
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
 
+        if(nx < 0 || nx >= N || ny < 0 || ny >= N || map[ny][nx] == 2){
+            Blue(x, y, dir, idx);
+        }else if(map[ny][nx] == 1){
+            Red(x, y, dir, idx);
+        }else {
+            White(x, y, dir, idx);
+        }
     }
 
 
