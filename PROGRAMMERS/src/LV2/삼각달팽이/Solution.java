062c8e97d1992d373d04f9f78521c714e81f6eef
Jeong-Wonseok <jws4858@gmail.com>
2024-07-09 21:59:03 +0900

    PROGRAMMERS LV2 삼각달팽이 finish

--- PROGRAMMERS/src/LV2/삼각달팽이/Solution.java
+++ PROGRAMMERS/src/LV2/삼각달팽이/Solution.java
@@ -1,18 +1,20 @@
 package LV2.삼각달팽이;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Solution {
 
-    public void main(String[] args){
+    public static void main(String[] args){
         solution(4);
+        System.out.println();
         solution(5);
+        System.out.println();
         solution(6);
     }
 
-    public int[] solution(int n) {
-        int[] answer = {};
+    public static int[] solution(int n) {
         int cnt = 1;
 
         int[][] snail = new int[n + 1][n + 1];
@@ -27,8 +29,16 @@ public class Solution {
             for(int j = 0; j < i-1 ; j++) {snail[row][++col] = curNum++;}
             for(int j = 0; j < i-2 ; j++) {snail[--row][--col] = curNum++;}
         }
-        for(int i = 0; i < n; i++)
-            System.out.println(Arrays.toString(snail[i]));
+
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i = 0; i <= n; i++)
+            for(int j = 0; j <= n; j++)
+                if(snail[i][j] != 0) ans.add(snail[i][j]);
+
+        int[] answer = new int[ans.size()];
+        for(int i = 0 ; i < ans.size(); i++){
+            answer[i] = ans.get(i);
+        }
         return answer;
     }
 }
